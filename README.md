60c9d272f328e3eec8f4498094a1e7abc0c3d57b
Svxy <aidan61605@gmail.com>
2023-04-20 13:37:26 -0400

    First release (wip)

---
+++ README.md
@@ -0,0 +1,5 @@
+<h2 align=center>Work in Progress</h2>
+
+<br>
+
+<p align=center>This script is my latest random project, a webscraper that in simple terms, scrapes the contents of each website listed in the config, and emails the user alerting them when said websites have recieved any updates in their source code using bs4. I havent tested this with other sites or pretty much at all yet, so make a pr if you get to it before I do : )</p>
---
+++ scraper.py
@@ -0,0 +1,80 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+import time
+from difflib import ndiff
+import logging
+import configparser
+
+logging.basicConfig(level=logging.INFO)
+
+config = configparser.ConfigParser()
+config.read('assets/config.ini')
+
+SENDER_EMAIL = config['email']['sender_email']
+SENDER_PASSWORD = config['email']['sender_password']
+RECIPIENT_EMAIL = config['email']['recipient_email']
+SMTP_SERVER = config['email']['smtp_server']
+
+def on_start():
+    print("\n\nScraper running!\nEverything is functioning properly.\n\n")
+    print(f"Sender email: {SENDER_EMAIL}")
+    print(f"Recipient email: {RECIPIENT_EMAIL}")
+    print(f"SMTP server: {SMTP_SERVER}\n")
+
+def get_website_files(url, path=''):
+    response = requests.get(url + path)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    files = []
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href.startswith('/'):
+            files.extend(get_website_files(url, href))
+        else:
+            files.append(path + href)
+    return files
+
+WEBSITE_LINKS = [link for link in config['websites'].values()]
+
+WEBSITE_CONTENTS = {}
+
+def get_website_contents(url):
+    response = requests.get(url)
+    return response.text
+
+def get_file_contents(url, file_path):
+    response = requests.get(url + file_path)
+    return response.text
+
+def send_notification(subject, body):
+    message = f'Subject: {subject}\n\n{body}'
+    with smtplib.SMTP(SMTP_SERVER, 587) as server:
+        server.ehlo()
+        server.starttls()
+        server.login(SENDER_EMAIL, SENDER_PASSWORD)
+        server.sendmail(SENDER_EMAIL, RECIPIENT_EMAIL, message)
+
+on_start()
+
+while True:
+    for url in WEBSITE_LINKS:
+        if url not in WEBSITE_CONTENTS:
+            WEBSITE_CONTENTS[url] = {}
+        for file_path in get_website_files(url):
+            if file_path not in WEBSITE_CONTENTS[url]:
+                contents = get_file_contents(url, file_path)
+                WEBSITE_CONTENTS[url][file_path] = contents
+            else:
+                current_contents = get_file_contents(url, file_path)
+                if WEBSITE_CONTENTS[url][file_path] != current_contents:
+                    changes = list(ndiff(WEBSITE_CONTENTS[url][file_path], current_contents))
+                    changes_str = '\n'.join(changes)
+                    subject = f'Update on {url}/{file_path}'
+                    body = f'The contents of {url}/{file_path} have changed.\n\nChanges:\n\n{changes_str}'
+                    try:
+                        send_notification(subject, body)
+                        logging.info('Email notification sent successfully.')
+                    except Exception as e:
+                        logging.error(f'Error sending email notification: {e}')
+                    WEBSITE_CONTENTS[url][file_path] = current_contents
+    time.sleep(15)
